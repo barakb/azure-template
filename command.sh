182e28febbfbf0cb631314e1d0372d3282c1687a
Vitalii Zinchenko <zinjvi@gmail.com>
2017-10-24 17:15:20 +0300

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-echo "hello!!1"
-echo "hello!!2" > ~/log.txt
+echo "Starting XAP..."
 
+/opt/gigaspaces-insightedge-12.2.0-ga-b18015-628/bin/gs-agent.sh --manager-local > /opt/xap.log 2>&1
+
+echo "Startup script completed!"
