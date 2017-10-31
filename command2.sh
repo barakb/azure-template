d0a380ea1c0350fcf079d4b7ba1bbb819ea58176
Vitalii Zinchenko <zinjvi@gmail.com>
2017-10-31 17:59:48 +0200

    Update command2.sh

--- command2.sh
+++ command2.sh
@@ -2,6 +2,6 @@
 
 echo "Starting XAP...2"
 
-nohup /opt/gigaspaces-insightedge-12.2.0-ga-b18015-628/bin/gs-agent.sh --manager > /opt/xap.log 2>&1 &
+nohup /opt/gigaspaces-xap/bin/gs-agent.sh --manager > /opt/xap.log 2>&1 &
 
 echo "Startup script completed!"
