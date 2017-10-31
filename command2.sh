40b63963ab6fa081ca93a5dbf29c7b83fc45d205
Vitalii Zinchenko <zinjvi@gmail.com>
2017-10-31 20:05:40 +0200

    Update command2.sh

--- command2.sh
+++ command2.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-echo "Starting XAP...3"
+echo "Starting XAP..."
+
+echo "XAP License key"
+
+XAP_LICENSE_KEY=$1
+
+export EXT_JAVA_OPTIONS="-Dcom.gs.licensekey=$XAP_LICENSE_KEY"
 
 nohup /opt/gigaspaces-xap/bin/gs-agent.sh --manager > /opt/xap.log 2>&1 &
 
