6a6cc812dfc9943fcd3ce27ba4ce019c34ee46ce
Vitalii Zinchenko <zinjvi@gmail.com>
2017-11-06 14:19:55 +0200

    Update install-worker.sh

--- install-worker.sh
+++ install-worker.sh
@@ -4,9 +4,11 @@ XAP_LICENSE_KEY=$1
 
 xap_blob="https://xapblob.blob.core.windows.net/xap/gigaspaces-xap-12.2.0-ga-b18000.zip"
 
-xap_home=/opt/gigaspaces-xap
+xap_folder_name=gigaspaces-xap-12.2.0-ga-b18000
+xap_home=/opt/$xap_folder_name
 xap_envs=$xap_home/bin/setenv-overrides.sh
 
+{
 echo ">> Installing required packages"
 sudo apt -y install unzip
 sudo apt -y install openjdk-8-jre-headless
@@ -21,8 +23,8 @@ echo ">> Setup env variables"
 echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" | sudo tee --append $xap_envs > /dev/null
 
 export EXT_JAVA_OPTIONS="-Dcom.gs.licensekey=$XAP_LICENSE_KEY"
-# export XAP_MANAGER_SERVERS=$(hostname)
+} > /opt/install.log 2>&1
 
-nohup /opt/gigaspaces-xap/bin/gs-agent.sh --gsc=1 > /opt/xap.log 2>&1 &
+nohup $xap_home/bin/gs-agent.sh --gsc=1 > /opt/xap.log 2>&1 &
 
 echo "Startup script completed!"
