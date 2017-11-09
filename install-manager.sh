a510b821d1d3cf33ad4d290945ea334ae3265eb6
vitaliy-zinchenko <zinjvi@gmail.com>
2017-11-09 15:42:38 +0200

    cloud-provider azure template

--- install-manager.sh
+++ install-manager.sh
@@ -1,23 +1,95 @@
 echo "Starting XAP..."
 
-export XAP_LICENSE_KEY=$1
-export SSH_PUBLIC_KEY=$2
+function print_input_parameter {
+    key=$1
+    value=$2
+    echo "  $key = $value"
+}
 
-xap_blob="https://xapblob.blob.core.windows.net/xap/gigaspaces-xap-12.2.0-ga-b18000.zip"
-xap_rest_blob="https://xapblob.blob.core.windows.net/xap/rest-api.jar"
+echo "Parsing input parameters..."
 
-xap_folder_name=gigaspaces-xap-12.2.0-ga-b18000
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    --xap-license)
+    export XAP_LICENSE_KEY=$2
+    print_input_parameter $key $2
+    shift
+    shift
+    ;;
+    --ssh-key)
+    export SSH_PUBLIC_KEY=$2
+    print_input_parameter $key $2
+    shift
+    shift
+    ;;
+    --azure-client-id)
+    export AZURE_AUTH_CLIENT_ID=$2
+    print_input_parameter $key $2
+    shift
+    shift
+    ;;
+    --azure-tenant-id)
+    export AZURE_AUTH_TENANT_ID=$2
+    print_input_parameter $key $2
+    shift
+    shift
+    ;;
+    --azure-client-secret)
+    export AZURE_AUTH_CLIENT_SECRET=$2
+    print_input_parameter $key $2
+    shift
+    shift
+    ;;
+    --azure-subscription-id)
+    export AZURE_AUTH_SUBSCRIPTION_ID=$2
+    print_input_parameter $key $2
+    shift
+    shift
+    ;;
+    --xap-blob-url)
+    export XAP_BLOB_URL=$2
+    print_input_parameter $key $2
+    shift
+    shift
+    ;;
+    --xap-rest-api-blob-url)
+    export XAP_REST_API_BLOB_URL=$2
+    print_input_parameter $key $2
+    shift
+    shift
+    ;;
+    *)
+    POSITIONAL+=("$1")
+    shift
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}"
+
+echo ""
+echo "Run installation"
+
+xap_folder_name=$(basename $XAP_BLOB_URL .zip)
 xap_home=/opt/$xap_folder_name
 xap_envs=$xap_home/bin/setenv-overrides.sh
 xap_plugins=$xap_home/lib/platform/manager/plugins
 
 {
+echo ">> Working variables:"
+echo "   - xap_folder_name = $xap_folder_name"
+echo "   - xap_home        = $xap_home"
+echo "   - xap_envs        = $xap_envs"
+echo "   - xap_plugins     = $xap_plugins"
+
 echo ">> Installing required packages"
 sudo apt -y install unzip
 sudo apt -y install openjdk-8-jdk-headless
 
 echo ">> Download XAP"
-sudo wget -q -O /opt/xap.zip $xap_blob
+sudo wget -q -O /opt/xap.zip $XAP_BLOB_URL
 
 echo ">> Unzipping Xap archive"
 sudo unzip -u -d /opt -q /opt/xap.zip
@@ -27,7 +99,7 @@ echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" | sudo tee --append $x
 
 echo ">> Copy Rest API jar to $xap_plugins"
 sudo mkdir -p $xap_plugins
-sudo wget -q -O $xap_plugins/rest-api.jar $xap_rest_blob
+sudo wget -q -O $xap_plugins/rest-api.jar $XAP_REST_API_BLOB_URL
 
 export EXT_JAVA_OPTIONS="-Dcom.gs.licensekey=$XAP_LICENSE_KEY"
 export XAP_WEBUI_OPTIONS="-Dcom.gs.licensekey=$XAP_LICENSE_KEY"
